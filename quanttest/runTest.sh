361ef5fc65f5b3406050bd7f61b89e2f8f535798
moonfalir <jonas.reynders@student.uhasselt.be>
2019-09-25 09:41:04 +0200

    Revert "Use locally build image for simulation"
    This reverts commit 13cd515b5e3095a2da69d27b865f0e2ee01c6c38.

--- quanttest/runTest.sh
+++ quanttest/runTest.sh
@@ -1,6 +1,6 @@
 CLIENT="quanttest" \
-CLIENT_PARAMS="-i eth0 -q /logs/clientquant.qlog http://192.168.100.100:4433/50000" \
+CLIENT_PARAMS="-i eth0 -q /logs/clientquant.log http://192.168.100.100:4433/500000" \
 SERVER="quanttest" \
-SERVER_PARAMS="-d ./ -i eth0 -q /logs/serverquant.qlog" \
-SCENARIO="droplist --delay=15ms --bandwidth=10Mbps --queue=25 --drops_to_client=1,3,4 --drops_to_server=5" \
+SERVER_PARAMS="-d ./ -i eth0 -q /logs/serverquant.log" \
+SCENARIO="simple-p2p --delay=15ms --bandwidth=10Mbps --queue=25" \
 docker-compose -f ../quic-network-simulator/docker-compose.yml up
