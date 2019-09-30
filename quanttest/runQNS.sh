aac1f4162a2e835a47d2b4b80352da8ab9195386
moonfalir <jonas.reynders@student.uhasselt.be>
2019-09-30 11:52:52 +0200

    Fix: change file type of log files to .qlog

--- quanttest/runQNS.sh
+++ quanttest/runQNS.sh
@@ -1,6 +1,6 @@
 CLIENT="quanttest" \
-CLIENT_PARAMS="-i eth0 -q /logs/clientquant.log http://192.168.100.100:4433/500000" \
+CLIENT_PARAMS="-i eth0 -q /logs/clientquant.qlog http://192.168.100.100:4433/50000" \
 SERVER="quanttest" \
-SERVER_PARAMS="-d ./ -i eth0 -q /logs/serverquant.log" \
+SERVER_PARAMS="-d ./ -i eth0 -q /logs/serverquant.qlog" \
 SCENARIO="simple-p2p --delay=15ms --bandwidth=10Mbps --queue=25" \
 docker-compose -f ../quic-network-simulator/docker-compose.yml up
