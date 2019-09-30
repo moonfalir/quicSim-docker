99585e9dc2b825ebdc2ffb90e4f750694cd4637f
moonfalir <jonas.reynders@student.uhasselt.be>
2019-09-30 11:57:24 +0200

    Finished p2p scenario: added args for customizing link, create volumes for log files

--- containernet/scenarios/simple-p2p/simple-p2p.py
+++ containernet/scenarios/simple-p2p/simple-p2p.py
@@ -5,14 +5,23 @@ from mininet.cli import CLI
 from mininet.link import TCLink, Intf
 from mininet.log import info, setLogLevel
 from os import environ
+from argparse import ArgumentParser
+from time import sleep
 
-if any(v not in environ for v in ['CLIENT', 'CLIENT_PARAMS', 'SERVER', 'SERVER']):
+sim_parser = ArgumentParser(description='Run mininet network simulator')
+sim_parser.add_argument('--delay', action='store', type=str, required=True, help='One-way delay of network, specify with units.')
+sim_parser.add_argument('--bandwidth', action='store', type=float, required=True, help='Bandwidth of the link in Mbit/s.')
+sim_parser.add_argument('--queue', action='store', type=int, required=True, help='Queue size of the queue attached to the link. Specified in packets.')
+sim_args = sim_parser.parse_args()
+
+if any(v not in environ for v in ['CLIENT', 'CLIENT_PARAMS', 'SERVER', 'SERVER', 'LOGDIR']):
     # TODO show help
     exit(1)
 client_image = environ['CLIENT']
 client_command = environ['CLIENT_PARAMS']
 server_image = environ['SERVER']
 server_command = environ['SERVER_PARAMS']
+logdir = environ['LOGDIR']
 
 setLogLevel('info')
 
@@ -22,13 +31,19 @@ net.addController('c0')
 info('*** Adding docker containers\n')
 server = net.addDocker('server', ip='10.0.0.251',
                        dimage=server_image + ":latest",
-                       dcmd=server_command)
-client = net.addDocker('client', ip='10.0.0.252', dimage=client_image + ":latest")
+                       dcmd=server_command,
+                       volumes=[logdir + '/logs/server:/logs'])
+client = net.addDocker('client', ip='10.0.0.252', 
+                       dimage=client_image + ":latest", 
+                       volumes=[logdir + '/logs/client:/logs'])
+
 info('*** Creating links\n')
-net.addLink(server, client, cls=TCLink, delay='100ms', bw=1)
+net.addLink(server, client, cls=TCLink, delay=sim_args.delay, bw=sim_args.bandwidth, max_queue_size=sim_args.queue)
 info('*** Starting network\n')
 net.start()
 info('\n' + client_command + '\n')
 info(client.cmd(client_command) + "\n")
+# Wait some time to allow server finish writing to log file
+sleep(5)
 info('*** Stopping network')
 net.stop()
--- quanttest/runMININET.sh
+++ quanttest/runMININET.sh
@@ -1,5 +1,7 @@
 CLIENT="quanttest" \
-CLIENT_PARAMS="./bin/client -i client-eth0 http://10.0.0.251:4433/500000" \
+CLIENT_PARAMS="./bin/client -i client-eth0 -q /logs/clientmnquant.qlog http://10.0.0.251:4433/50000" \
 SERVER="quanttest" \
-SERVER_PARAMS="./bin/server -d ./ -i server-eth0" \
+SERVER_PARAMS="./bin/server -d ./ -i server-eth0 -q /logs/servermnquant.qlog" \
+SCENARIO="--delay 15ms --bandwidth 10 --queue 25" \
+LOGDIR="$PWD" \
 docker-compose -f ../containernet/docker-compose.yml up
