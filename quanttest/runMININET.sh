c1dcbb0dbff5d1df0143c5f257d62fc35704cd9c
moonfalir <jonas.reynders@student.uhasselt.be>
2019-09-30 12:11:11 +0200

    Generate log file names with timestamps

--- quanttest/runMININET.sh
+++ quanttest/runMININET.sh
@@ -1,7 +1,8 @@
+CURTIME=$(date +%Y-%m-%d-%H-%M) \
 CLIENT="quanttest" \
-CLIENT_PARAMS="./bin/client -i client-eth0 -q /logs/clientmnquant.qlog http://10.0.0.251:4433/50000" \
+CLIENT_PARAMS="./bin/client -i client-eth0 -q /logs/clientmnquant_$CURTIME.qlog http://10.0.0.251:4433/50000" \
 SERVER="quanttest" \
-SERVER_PARAMS="./bin/server -d ./ -i server-eth0 -q /logs/servermnquant.qlog" \
+SERVER_PARAMS="./bin/server -d ./ -i server-eth0 -q /logs/servermnquant_$CURTIME.qlog" \
 SCENARIO="--delay 15ms --bandwidth 10 --queue 25" \
 LOGDIR="$PWD" \
 docker-compose -f ../containernet/docker-compose.yml up
--- quanttest/runQNS.sh
+++ quanttest/runQNS.sh
@@ -1,6 +1,7 @@
+CURTIME=$(date +%Y-%m-%d-%H-%M) \
 CLIENT="quanttest" \
-CLIENT_PARAMS="-i eth0 -q /logs/clientquant.qlog http://192.168.100.100:4433/50000" \
+CLIENT_PARAMS="-i eth0 -q /logs/clientquant_$CURTIME.qlog http://192.168.100.100:4433/50000" \
 SERVER="quanttest" \
-SERVER_PARAMS="-d ./ -i eth0 -q /logs/serverquant.qlog" \
+SERVER_PARAMS="-d ./ -i eth0 -q /logs/serverquant_$CURTIME.qlog" \
 SCENARIO="simple-p2p --delay=15ms --bandwidth=10Mbps --queue=25" \
 docker-compose -f ../quic-network-simulator/docker-compose.yml up
