2809a695563ff2ef22f2c3204eb6aed7a68cf76b
moonfalir <jonas.reynders@student.uhasselt.be>
2019-10-01 10:10:49 +0200

    Added option to start blackhole scenario

---
+++ containernet/scenarios/blackhole/blackhole.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+from mininet.net import Containernet
+from mininet.node import Controller
+from mininet.cli import CLI
+from mininet.link import TCLink, Intf
+from mininet.log import info, setLogLevel
+from os import environ
+from argparse import ArgumentParser
+from time import sleep
+
+class Blackhole:
+    def addCLIArguments(self, blackhole_parser):
+        blackhole_parser.add_argument('--delay', action='store', type=str, required=True, help='One-way delay of network, specify with units.')
+        blackhole_parser.add_argument('--bandwidth', action='store', type=float, required=True, help='Bandwidth of the link in Mbit/s.')
+        blackhole_parser.add_argument('--queue', action='store', type=int, required=True, help='Queue size of the queue attached to the link. Specified in packets.')
+        blackhole_parser.add_argument('--on', action='store', type=int, required=True, help='Time period that traffic is allowed to flow.')
+        blackhole_parser.add_argument('--off', action='store', type=int, required=True, help='Time period that traffic is blocked.')
+        blackhole_parser.add_argument('--repeat', action='store', type=int, required=False, help='Repeat blocking and unblocking of traffic')
+        blackhole_parser.add_argument('--direction', action='store', type=int, required=False, help='Specifiy the direction in which to block traffic.')
+
+    def run(self, sim_args):
+        if any(v not in environ for v in ['CLIENT', 'CLIENT_PARAMS', 'SERVER', 'SERVER', 'LOGDIR']):
+            # TODO show help
+            exit(1)
+        client_image = environ['CLIENT']
+        client_command = environ['CLIENT_PARAMS']
+        server_image = environ['SERVER']
+        server_command = environ['SERVER_PARAMS']
+        logdir = environ['LOGDIR']
+
+        setLogLevel('info')
+
+        net = Containernet(controller=Controller)
+        info('*** Adding controller\n')
+        net.addController('c0')
+        info('*** Adding docker containers\n')
+        server = net.addDocker('server', ip='10.0.0.251',
+                               dimage=server_image + ":latest",
+                               dcmd=server_command,
+                               volumes=[logdir + '/logs/server:/logs'])
+        client = net.addDocker('client', ip='10.0.0.252', 
+                               dimage=client_image + ":latest", 
+                               volumes=[logdir + '/logs/client:/logs'])
+
+        info('*** Creating links\n')
+        p2p_link = net.addLink(server, client, cls=TCLink, delay=sim_args.delay, bw=sim_args.bandwidth, max_queue_size=sim_args.queue)
+        info('*** Starting network\n')
+        net.start()
+        info('\n' + client_command + '\n')
+        info(client.cmd(client_command) + "\n")
+        # Wait some time to allow server finish writing to log file
+        sleep(5)
+        info('*** Stopping network')
+        net.stop()
--- containernet/scenarios/containernet.py
+++ containernet/scenarios/containernet.py
@@ -4,14 +4,22 @@ import sys
 import os
 
 curdir = os.path.dirname(os.path.realpath(__file__))
-
 sim_parser = ArgumentParser(description='Run mininet network simulator')
 subparsers = sim_parser.add_subparsers(dest="scenario")
-p2p_parser = subparsers.add_parser('simple_p2p')
+
+#Add simple p2p arguments
+sub_parser = subparsers.add_parser('simple_p2p')
 sys.path.append(curdir + '/simple_p2p')
 from simple_p2p import Simple_p2p
 p2p = Simple_p2p()
-p2p.addCLIArguments(p2p_parser)
+p2p.addCLIArguments(sub_parser)
+
+#Add blackhole arguments
+sub_parser = subparsers.add_parser('blackhole')
+sys.path.append(curdir + '/blackhole')
+from blackhole import Blackhole
+blackhole = Blackhole()
+blackhole.addCLIArguments(sub_parser)
 
 sim_args = sim_parser.parse_args()
 
@@ -21,8 +29,13 @@ def run_simple_p2p():
     p2p = Simple_p2p()
     p2p.run(sim_args)
 
+def run_blackhole():
+    blackhole = Blackhole()
+    blackhole.run(sim_args)
+
 switch = {
-    'simple_p2p': run_simple_p2p
+    'simple_p2p': run_simple_p2p,
+    'blackhole': run_blackhole
 }
 func = switch.get(sim_args.scenario)
 func()
