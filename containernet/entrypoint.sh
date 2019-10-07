d68b7b257e1bb2d42b2fd4a1a0b408ab5c934afb
moonfalir <jonas.reynders@student.uhasselt.be>
2019-10-07 14:22:48 +0200

    Finished droplist scenario: use custom POX controller, pass droplist arguments to controller

--- containernet/entrypoint.sh
+++ containernet/entrypoint.sh
@@ -12,12 +12,8 @@ if [ ! -S /var/run/docker.sock ]; then
     exit 1
 fi
 
-# this cannot be done from the Dockerfile since we have the socket not mounted during build
-set +e
-echo 'Pulling the "ubuntu:trusty" and "ubuntu:xenial" image for later use...'
-docker pull 'ubuntu:trusty'
-docker pull 'ubuntu:xenial'
-set -e
+# Copy custom controllers to pox directory
+cp -a /containernet/poxcontrollers/. /pox/pox/forwarding/
 
 echo "Welcome to Containernet running within a Docker container ..."
 
