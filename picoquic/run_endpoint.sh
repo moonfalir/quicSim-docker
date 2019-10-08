ff5a9cefe5d3b6fdb07e0da2ca547894cabbb155
moonfalir <jonas.reynders@student.uhasselt.be>
2019-10-08 13:24:49 +0200

    Added Picoquic docker files

---
+++ picoquic/run_endpoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set up the routing needed for the simulation
+/setup.sh
+
+# The following variables are available for use:
+# - ROLE contains the role of this execution context, client or server
+# - SERVER_PARAMS contains user-supplied command line parameters
+# - CLIENT_PARAMS contains user-supplied command line parameters
+
+if [ "$ROLE" == "client" ]; then
+    ./wait-for-it.sh sim:57832 -s -t 30    
+    python3 examples/http3_client.py -v $CLIENT_PARAMS
+elif [ "$ROLE" == "server" ]; then
+    python3 examples/http3_server.py -v $SERVER_PARAMS
+fi
