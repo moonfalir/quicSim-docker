a743c4aa56ed86700f3e635db24949f94105fc13
moonfalir <jonas.reynders@student.uhasselt.be>
2019-10-21 14:05:57 +0200

    Updated ip addresses used for implementations

--- aioquic/runQNS.sh
+++ aioquic/runQNS.sh
@@ -1,7 +1,8 @@
 CURTIME=$(date +%Y-%m-%d-%H-%M) \
 CLIENT="aioquic" \
-CLIENT_PARAMS="--ca-certs tests/pycacert.pem -q /logs/clientaioquic_$CURTIME.qlog --legacy-http https://192.168.100.100:4433/50000" \
+CLIENT_PARAMS="--ca-certs tests/pycacert.pem -q /logs/clientaioquic_$CURTIME.qlog --legacy-http https://193.167.100.100:4433/5000000" \
 SERVER="aioquic" \
-SERVER_PARAMS="--certificate tests/ssl_cert.pem --private-key tests/ssl_key.pem --host 192.168.100.100 -q /logs" \
+SERVER_PARAMS="--certificate tests/ssl_cert.pem --private-key tests/ssl_key.pem --host 193.167.100.100 -q /logs" \
 SCENARIO="simple-p2p --delay=15ms --bandwidth=10Mbps --queue=25" \
+LOGDIR="$PWD/logs" \
 docker-compose -f ../quic-network-simulator/docker-compose.yml up --abort-on-container-exit
--- ngtcp2/runQNS.sh
+++ ngtcp2/runQNS.sh
@@ -1,8 +1,8 @@
 CURTIME=$(date +%Y-%m-%d-%H-%M) \
 CLIENT="ngtcp2" \
-CLIENT_PARAMS="--timeout=1000 -q --qlog-file=/logs/clientngtcp2_$CURTIME.qlog 193.167.100.100 4433 http://server/500000" \
+CLIENT_PARAMS="--timeout=1000 -q --qlog-file=/logs/clientngtcp2_$CURTIME.qlog 193.167.100.100 4433 http://server/5000000" \
 SERVER="ngtcp2" \
 SERVER_PARAMS="--qlog-dir=/logs -q 193.167.100.100 4433 server.key server.crt" \
-SCENARIO="simple-p2p --delay=25ms --bandwidth=5Mbps --queue=25" \
+SCENARIO="simple-p2p --delay=15ms --bandwidth=10Mbps --queue=25" \
 LOGDIR="$PWD/logs" \
 docker-compose -f ../quic-network-simulator/docker-compose.yml up --abort-on-container-exit
