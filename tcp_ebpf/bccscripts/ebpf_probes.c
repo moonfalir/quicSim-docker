ac9796cd8c12d52de23ac48693542b524192d4e6
moonfalir <jonas.reynders@student.uhasselt.be>
2019-10-30 13:38:30 +0100

    Added state change probe

--- tcp_ebpf/bccscripts/ebpf_probes.c
+++ tcp_ebpf/bccscripts/ebpf_probes.c
@@ -4,38 +4,6 @@
 #include <bcc/proto.h>
 #include <net/tcp.h>
 
-struct bbr {
-	u32	min_rtt_us;	        /* min RTT in min_rtt_win_sec window */
-	u32	min_rtt_stamp;	        /* timestamp of min_rtt_us */
-	u32	probe_rtt_done_stamp;   /* end time for BBR_PROBE_RTT mode */
-	u32	rtt_cnt;	    /* count of packet-timed rounds elapsed */
-	u32     next_rtt_delivered; /* scb->tx.delivered at end of round */
-	u64	cycle_mstamp;	     /* time of this cycle phase start */
-	u32     mode:3,		     /* current bbr_mode in state machine */
-		prev_ca_state:3,     /* CA state on previous ACK */
-		packet_conservation:1,  /* use packet conservation? */
-		round_start:1,	     /* start of packet-timed tx->ack round? */
-		idle_restart:1,	     /* restarting after idle? */
-		probe_rtt_round_done:1,  /* a BBR_PROBE_RTT round at 4 pkts? */
-		unused:13,
-		lt_is_sampling:1,    /* taking long-term ("LT") samples now? */
-		lt_rtt_cnt:7,	     /* round trips in long-term interval */
-		lt_use_bw:1;	     /* use lt_bw as our bw estimate? */
-	u32	lt_bw;		     /* LT est delivery rate in pkts/uS << 24 */
-	u32	lt_last_delivered;   /* LT intvl start: tp->delivered */
-	u32	lt_last_stamp;	     /* LT intvl start: tp->delivered_mstamp */
-	u32	lt_last_lost;	     /* LT intvl start: tp->lost */
-	u32	pacing_gain:10,	/* current gain for setting pacing rate */
-		cwnd_gain:10,	/* current gain for setting cwnd */
-		full_bw_reached:1,   /* reached full bw in Startup? */
-		full_bw_cnt:2,	/* number of rounds without large bw gains */
-		cycle_idx:3,	/* current index in pacing_gain cycle array */
-		has_seen_rtt:1, /* have we seen an RTT sample yet? */
-		unused_b:5;
-	u32	prior_cwnd;	/* prior cwnd upon entering loss recovery */
-	u32	full_bw;	/* recent bw, to estimate if pipe is full */
-};
-
 struct congestion_info {
 	u64 timestamp;
 	u32 saddr;
@@ -50,7 +18,17 @@ struct congestion_info {
 	u64 bytes_acked;
 	u32 ssthresh;
 };
+
+struct ca_init_info {
+    u32 saddr;
+	u32 daddr;
+	u16 sport;
+    u16 dport;
+    u64 timestamp;
+    u8 new_state;
+};
 BPF_PERF_OUTPUT(tcp_events);
+BPF_PERF_OUTPUT(ca_state);
 
 
 void trace_rcv_established(struct pt_regs *ctx, struct sock *sk)
@@ -77,6 +55,18 @@ void trace_rcv_established(struct pt_regs *ctx, struct sock *sk)
 	}
 }
 
-void trace_statechange(struct pt_regs *ctx, struct sock *sk) {
+void trace_cubictcp_state(struct pt_regs *ctx, struct sock *sk, u8 new_state) {
+    u16 family = sk->__sk_common.skc_family;
+	if (family == AF_INET) {
+        struct ca_init_info info = {};
+        info.saddr = sk->__sk_common.skc_rcv_saddr;
+		info.daddr = sk->__sk_common.skc_daddr;
+		info.sport = sk->__sk_common.skc_num;
+		u16 dport = sk->__sk_common.skc_dport;
+		info.dport = ntohs(dport);
+        info.new_state = new_state;
 
+        info.timestamp = bpf_ktime_get_ns();
+    	ca_state.perf_submit(ctx, &info, sizeof(info));
+	}
 }
--- tcp_ebpf/bccscripts/tcpprobe_congestion.py
+++ tcp_ebpf/bccscripts/tcpprobe_congestion.py
@@ -10,7 +10,8 @@ import ctypes
 
 # initialize BPF
 b = BPF(src_file="ebpf_probes.c")
-b.attach_kprobe(event="tcp_rcv_established", fn_name="trace_rcv_established")
+#b.attach_kprobe(event="tcp_rcv_established", fn_name="trace_rcv_established")
+b.attach_kprobe(event="bictcp_state", fn_name="trace_cubictcp_state")
 qlog = {
 	"qlog_version": "draft-01",
 	"traces": [
@@ -48,6 +49,13 @@ qlog = {
 		}
 	]
 }
+
+ca_states = {}
+ca_states[0] = "TCP_CA_Open"
+ca_states[1] = "TCP_CA_Disorder"
+ca_states[2] = "TCP_CA_CWR"
+ca_states[3] = "TCP_CA_Recovery"
+ca_states[4] = "TCP_CA_Loss"
 
 with open('/proc/uptime', 'r') as f:
 	uptime_s = float(f.readline().split()[0])
@@ -98,16 +106,32 @@ def print_tcp_event(cpu, data, size):
 			}
 		)
 		qlog["traces"][1]["events"].append(output_arr)
+
+def print_ca_state(cpu, data, size):
+	global ca_states
+	event = b["ca_state"].event(data)
+	sender = inet_ntop(AF_INET, pack('I', event.saddr)) + ":" + str(event.sport)
+	receiver = inet_ntop(AF_INET, pack('I', event.daddr)) + ":" + str(event.dport)
+	if sender.__contains__("10.0.0.252"):
+		global reference_time_s
+		print("client new state: " + ca_states[event.new_state])
+	if sender.__contains__("10.0.0.251"):
+		global reference_time_c
+		print("server new state: " + ca_states[event.new_state])
+
+
+
 print("Tracing tcp events ... Hit Ctrl-C to end")
 
 # header
 print("Output network information ...")
 
 b["tcp_events"].open_perf_buffer(print_tcp_event)
+b["ca_state"].open_perf_buffer(print_ca_state)
 while 1:
     try:
         b.perf_buffer_poll()
     except KeyboardInterrupt:
-		with open('/scripts/' + str(ti.time()) + '.qlog', 'w') as f:
-			f.write(json.dumps(qlog))
+		#with open('/scripts/' + str(ti.time()) + '.qlog', 'w') as f:
+		#	f.write(json.dumps(qlog))
 		exit()
