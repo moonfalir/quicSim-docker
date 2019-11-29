12ff5178c87e8da26a1b77c885d980dd53573aa3
moonfalir <jonas.reynders@student.uhasselt.be>
2019-11-29 15:35:26 +0100

    Fixed build process of picoquic + added scripts for mininet

--- picoquic/runQNS.sh
+++ picoquic/runQNS.sh
@@ -1,8 +1,8 @@
 CURTIME=$(date +%Y-%m-%d-%H-%M) \
 CLIENT="picoquic" \
-CLIENT_PARAMS="-b /logs/clientpico_qns.log 192.168.100.100 4443" \
+CLIENT_PARAMS="-L -b /logs/clientpico_qns.log 193.167.100.100 4443 0:/5000000;" \
 SERVER="picoquic" \
-SERVER_PARAMS="-1 -b /logs/serverpico_qns.log" \
-SCENARIO="simple-p2p --delay=25ms --bandwidth=1Mbps --queue=25" \
+SERVER_PARAMS="-1 -L -b /logs/serverpico_qns.log" \
+SCENARIO="simple-p2p --delay=25ms --bandwidth=5Mbps --queue=25" \
 LOGDIR="../picoquic/logs" \
-docker-compose -f ../quic-network-simulator/docker-compose.yml up #--abort-on-container-exit
+docker-compose -f ../quic-network-simulator/docker-compose.yml up --abort-on-container-exit
