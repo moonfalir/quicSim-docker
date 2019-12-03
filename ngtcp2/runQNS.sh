31ccd0ae0aecaa9c020e9dc7e0258fec76545895
moonfalir <jonas.reynders@student.uhasselt.be>
2019-12-03 12:02:51 +0100

    Update code on container start + allow build from commit

--- ngtcp2/runQNS.sh
+++ ngtcp2/runQNS.sh
@@ -1,8 +1,10 @@
 CURTIME=$(date +%Y-%m-%d-%H-%M) \
 CLIENT="ngtcp2" \
-CLIENT_PARAMS="--timeout=1000 -q --qlog-file=/logs/clientngtcp2_$CURTIME.qlog 193.167.100.100 4433 http://server/5000000" \
+CLIENT_PARAMS="--timeout=1000 -q --qlog-file=/logs/clientngtcp2_$CURTIME.qlog 193.167.100.100 4433 http://test/5000000" \
 SERVER="ngtcp2" \
 SERVER_PARAMS="--qlog-dir=/logs -q 193.167.100.100 4433 server.key server.crt" \
-SCENARIO="simple-p2p --delay=15ms --bandwidth=10Mbps --queue=25" \
+SCENARIO="simple-p2p --delay=15ms --bandwidth=5Mbps --queue=25" \
 LOGDIR="$PWD/logs" \
+CL_COMMIT="" \
+SV_COMMIT="" \
 docker-compose -f ../quic-network-simulator/docker-compose.yml up --abort-on-container-exit
--- ngtcp2/run_endpoint.sh
+++ ngtcp2/run_endpoint.sh
@@ -3,7 +3,13 @@
 # Set up the routing needed for the simulation
 /setup.sh
 
-git pull && autoreconf -i && ./configure PKG_CONFIG_PATH=$PWD/../openssl/build/lib/pkgconfig:$PWD/../nghttp3/build/lib/pkgconfig \
+git pull
+
+if [ "$COMMIT" ]; then 
+    git checkout "$COMMIT"
+fi
+
+autoreconf -i && ./configure PKG_CONFIG_PATH=$PWD/../openssl/build/lib/pkgconfig:$PWD/../nghttp3/build/lib/pkgconfig \
  LDFLAGS="-Wl,-rpath,$PWD/../openssl/build/lib" && make -j$(nproc) check
 
 # The following variables are available for use:
