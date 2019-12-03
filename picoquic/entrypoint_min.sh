57f548fe4ec395173e2b45b623fd51c7de97fec3
moonfalir <jonas.reynders@student.uhasselt.be>
2019-12-03 12:23:08 +0100

    Update code on container start + build from commit

--- picoquic/entrypoint_min.sh
+++ picoquic/entrypoint_min.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+git pull
+
+if [ "$COMMIT" ]; then 
+    git checkout "$COMMIT"
+fi
+
+cmake . && make
+
 # The following variables are available for use:
 # - ROLE contains the role of this execution context, client or server
 # - SERVER_PARAMS contains user-supplied command line parameters
@@ -7,10 +15,10 @@
 
 if [ "$ROLE" == "client" ]; then 
     ./picoquicdemo $CLIENT_PARAMS
-    ./picolog_t -f qlog /logs/clientpico_qns.log > /logs/clientpico_qns.qlog 
+    ./picolog_t -f qlog /logs/clientpico_mn.log > /logs/clientpico_mn.qlog 
     sleep 3
 elif [ "$ROLE" == "server" ]; then
     ./picoquicdemo $SERVER_PARAMS
-    ./picolog_t -f qlog /logs/serverpico_qns.log > /logs/serverpico_qns.qlog
+    ./picolog_t -f qlog /logs/serverpico_mn.log > /logs/serverpico_mn.qlog
     sleep 3
 fi
--- picoquic/runMININET.sh
+++ picoquic/runMININET.sh
@@ -1,8 +1,10 @@
 CURTIME=$(date +%Y-%m-%d-%H-%M) \
 CLIENT="picoquic" \
-CLIENT_PARAMS="-L -b /logs/clientpico_qns.log 10.0.0.251 4443 0:/5000000;" \
+CLIENT_PARAMS="-L -b /logs/clientpico_mn.log 10.0.0.251 4443 0:/5000000;" \
 SERVER="picoquic" \
-SERVER_PARAMS="-1 -L -b /logs/serverpico_qns.log" \
+SERVER_PARAMS="-1 -L -b /logs/serverpico_mn.log" \
 SCENARIO="simple_p2p --delay=15ms --bandwidth=5 --queue=25" \
 LOGDIR="$PWD" \
+CL_COMMIT="" \
+SV_COMMIT="" \
 docker-compose -f ../containernet/docker-compose.yml up
