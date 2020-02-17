8f246cee1975af744af5fb6cfab2ac795308b398
moonfalir <jonas.reynders@student.uhasselt.be>
2020-02-17 15:55:58 +0100

    Use fork of aioquic

--- aioquic/runMININET.sh
+++ aioquic/runMININET.sh
@@ -1,9 +1,9 @@
 CURTIME=$(date +%Y-%m-%d-%H-%M) \
 CLIENT="aioquic" \
-CLIENT_PARAMS="--ca-certs tests/pycacert.pem -q /logs/clientaioquic_$CURTIME.qlog --legacy-http https://10.0.0.251:4433/5000000" \
+CLIENT_PARAMS="--ca-certs tests/pycacert.pem -q /logs/clientaioquic_$CURTIME.qlog -l /logs/ssl-key.log --legacy-http https://10.0.0.251:4433/5000000" \
 SERVER="aioquic" \
 SERVER_PARAMS="--certificate tests/ssl_cert.pem --private-key tests/ssl_key.pem -q /logs" \
-SCENARIO="blackhole --delay 15ms --bandwidth 5 --queue 25 --on 3 --off 1 --repeat 5" \
+SCENARIO="simple_p2p --delay 15ms --bandwidth 5 --queue 25" \
 CLIENT_LOGS="$PWD/logs" \
 SERVER_LOGS="$PWD/logs" \
 CL_COMMIT="" \
