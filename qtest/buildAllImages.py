34346a9e3f11598bd23c9437a6e40c20b47f1b6d
moonfalir <jonas.reynders@student.uhasselt.be>
2020-06-02 17:28:51 +0200

    Added prints for building each docker image

--- qtest/buildAllImages.py
+++ qtest/buildAllImages.py
@@ -2,12 +2,19 @@ import docker
 dockercl = docker.from_env()
 
 
+print("build QNS endpoint")
 dockercl.images.build(path="../quic-network-simulator/endpoint", tag="quic-network-simulator-endpoint")
+print("build QNS sim")
 dockercl.images.build(path="../quic-network-simulator/sim", tag="sim")
+print("build wireshark")
 dockercl.images.build(path="../wireshark", tag="qtest-tshark")
 
+print("build MIN sim")
 dockercl.images.build(path="../containernet", tag="containernet")
 
+print("build Quant")
 dockercl.images.build(path="../quant", tag="quant")
+print("build Aioquic")
 dockercl.images.build(path="../aioquic", tag="aioquic")
+print("build Tcp eBPF")
 dockercl.images.build(path="../tcpebpf", tag="tcpebpf")
