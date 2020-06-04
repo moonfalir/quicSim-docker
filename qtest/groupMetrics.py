b5d6f14ba275709eaf06736f8890baea63c876f5
moonfalir <jonas.reynders@student.uhasselt.be>
2020-06-04 17:09:27 +0200

    groups metric files from different servers into 1

---
+++ qtest/groupMetrics.py
@@ -0,0 +1,91 @@
+import sys, os, re, json
+
+def getMedianValues(runs: list):
+        rtts = []
+        cwnds = []
+        goodputs = []
+        throughputs = []
+        medians = {}
+
+        for run in runs:
+            rtts.append(run["avg_rtt"])
+            cwnds.append(run["avg_cwnd"])
+            goodputs.append(run["avg_goodput"])
+            throughputs.append(run["avg_throughput"])
+
+        rtts.sort()
+        cwnds.sort()
+        goodputs.sort()
+        throughputs.sort()
+
+        middle = int(len(rtts) / 2)
+        if len(rtts) == 1:
+            middle = 0
+
+        medians["mdn_rtt"] = rtts[middle]
+        medians["mdn_cwnd"] = cwnds[middle]
+        medians["mdn_goodput"] = goodputs[middle]
+        medians["mdn_throughput"] = throughputs[middle]
+
+        return medians
+
+def groupMetrics(logdir: str):
+    print("grouping metrics")
+    metrics = []
+    # get all "metrics.json" files
+    metricfiles = getMetricFiles(logdir)
+    # loop through files
+    for mfile in metricfiles:
+        mdata = open(mfile, "r")
+        mdata = json.load(mdata)
+        # for each entry see if it exists
+        for testcase in mdata:
+            mindex = next((i for i, item in enumerate(metrics) if item['name'] == testcase['name'] and item['sim'] == testcase['sim']), -1)
+            # if not add entry to dict
+            if mindex == -1:
+                metrics.append({
+                    "name": testcase['name'],
+                    "sim": testcase['sim'],
+                    "mdn_goodput": 0.0,
+                    "mdn_throughput": 0.0,
+                    "mdn_rtt": 0.0,
+                    "mdn_cwnd": 0.0,
+                    "runs": []
+                })
+                mindex = len(metrics) - 1
+            # add runs to entry in dict
+            runs = testcase['runs']
+            metrics[mindex]['runs'].extend(runs)
+    # loop through each entry
+    for id in range(0, len(metrics)):
+        # find median for each value
+        medians = getMedianValues(metrics[id]['runs'])
+        metrics[id]["mdn_goodput"] = medians["mdn_goodput"]
+        metrics[id]["mdn_throughput"] = medians["mdn_throughput"]
+        metrics[id]["mdn_rtt"] = medians["mdn_rtt"]
+        metrics[id]["mdn_cwnd"] = medians["mdn_cwnd"]
+    with open(logdir + "/metrics.json", mode='w') as metrics_file:
+        json.dump(metrics, metrics_file)
+
+def getMetricFiles(logdir: str):
+    regex = re.compile("metrics.\.json")
+    metricfiles = []
+    # find converted pcap files
+    for dirpath, dirnames, filenames in os.walk(logdir):
+        for f in filenames:
+            if regex.match(f):
+                metricfiles.append(os.path.join(dirpath, f))
+    
+    return metricfiles
+
+if len(sys.argv) < 2:
+    print("insert directory name of collected log files")
+    exit(0)
+
+logdir = os.path.dirname(os.path.abspath(__file__)) + "/logs/" + sys.argv[1]
+
+if os.path.isdir(logdir):
+    groupMetrics(logdir)
+else:
+    print("directory does not exist")
+    exit(0)
