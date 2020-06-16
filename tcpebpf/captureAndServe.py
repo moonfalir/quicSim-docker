73c3b7272a8f90c0c589479e0054c06fb416a193
moonfalir <jonas.reynders@student.uhasselt.be>
2020-06-16 12:59:43 +0200

    Longer delay to allow eBPF to be compiled

--- tcpebpf/captureAndServe.py
+++ tcpebpf/captureAndServe.py
@@ -3,7 +3,7 @@ import subprocess, os, signal, time
 # start eBPF capture
 ebpf = subprocess.Popen(["python", "/bccscripts/tcpprobe_congestion.py"])
 
-time.sleep(2)
+time.sleep(5)
 params = os.environ["SERVER_PARAMS"]
 # start iperf transfer
 subprocess.run("iperf3 " + params, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
