c9e668d9d10728f5829d36dfd313a264425c089b
moonfalir <jonas.reynders@student.uhasselt.be>
2020-06-28 14:12:47 +0200

    updated run scripts

--- aioquic/runMININET.sh
+++ aioquic/runMININET.sh
@@ -1,6 +1,6 @@
 CURTIME=$(date +%Y-%m-%d-%H-%M) \
 CLIENT="aioquic" \
-CLIENT_PARAMS="--ca-certs tests/pycacert.pem -q /logs/clientaioquic_$CURTIME.qlog -l /logs/ssl-key.log --legacy-http https://10.0.0.251:4433/5000000" \
+CLIENT_PARAMS="--ca-certs tests/pycacert.pem -l /logs/ssl-key.log -q /logs --legacy-http https://10.0.0.251:4433/5000000" \
 SERVER="aioquic" \
 SERVER_PARAMS="--certificate tests/ssl_cert.pem --private-key tests/ssl_key.pem -q /logs" \
 SCENARIO="simple_p2p --delay 15ms --bandwidth 5 --queue 25" \
--- aioquic/runQNS.sh
+++ aioquic/runQNS.sh
@@ -1,8 +1,8 @@
 CURTIME=$(date +%Y-%m-%d-%H-%M) \
 CLIENT="aioquic" \
-CLIENT_PARAMS="--ca-certs tests/pycacert.pem -q /logs/clientaioquic_$CURTIME.qlog -l /logs/ssl-key.log --legacy-http https://193.167.100.100:4433/5000000" \
+CLIENT_PARAMS="--ca-certs tests/pycacert.pem -l /logs/ssl-key.log --quic-log /logs --legacy-http https://193.167.100.100:4433/5000000" \
 SERVER="aioquic" \
-SERVER_PARAMS="--certificate tests/ssl_cert.pem --private-key tests/ssl_key.pem --host 193.167.100.100 -q /logs" \
+SERVER_PARAMS="--certificate tests/ssl_cert.pem --private-key tests/ssl_key.pem --host 193.167.100.100 --quic-log /logs" \
 SCENARIO="simple-p2p --delay=15ms --bandwidth=5Mbps --queue=25" \
 CLIENT_LOGS="$PWD/logs" \
 SERVER_LOGS="$PWD/logs" \
